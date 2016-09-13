90f8f69a8d9f439bbfd35cbc6c8a50139a87edab
chris48s <c.mshaw@yahoo.co.uk>
2016-09-13 20:28:34 +0100

    update to match new base scraper format

--- scraper.py
+++ scraper.py
@@ -1,4 +1,4 @@
-from geojson_scraper import scrape
+from dc_base_scrapers.geojson_scraper import GeoJsonScraper
 
 
 stations_url = "http://inspire.eden.gov.uk/inspireeden/EDEN/wms?service=WFS&version=1.3.0&request=GetFeature&typeNames=PollingStations&outputFormat=json&srsName=EPSG%3A4326"
@@ -6,5 +6,7 @@ districts_url = "http://inspire.eden.gov.uk/inspireeden/EDEN/wms?service=WFS&ver
 council_id = 'E07000030'
 
 
-scrape(stations_url, council_id, 'utf-8', 'stations')
-scrape(districts_url, council_id, 'utf-8', 'districts')
+stations_scraper = GeoJsonScraper(stations_url, council_id, 'utf-8', 'stations')
+stations_scraper.scrape()
+districts_scraper = GeoJsonScraper(districts_url, council_id, 'utf-8', 'districts')
+districts_scraper.scrape()
